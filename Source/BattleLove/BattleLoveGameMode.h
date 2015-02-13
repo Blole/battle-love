79b7374dd17804512f16215f8ed9835b210b9c98
Carl Svensson <calle.svensson@zeta-two.com>
2015-02-13 20:23:46 +0100

    Initial commit

---
+++ Source/BattleLove/BattleLoveGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "BattleLoveGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLELOVE_API ABattleLoveGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
